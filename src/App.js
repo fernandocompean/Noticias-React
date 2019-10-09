5aa1b697b45907834431ce1ece95d32f9cb7d01d
Macbook Air de Luis Fernando <Fer@Fernandos-MacBook-Air.local>
2019-10-09 16:44:00 -0500

    62. Consultando la API de noticias

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-  state = {  }
+  state = { 
+    noticias: []
+   }
+
+  componentDidMount() {
+
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&apiKey=f418e137e59e447a8c1a480f863c18f2`;
+
+    const resp = await fetch(url);
+    const noticias = await resp.json();
+
+    this.setState({
+      noticias: noticias.articles
+    })
+  }
+
   render() {
     return (
       <h1>Noticias</h1>
